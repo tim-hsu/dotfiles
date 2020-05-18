696f612f62656e8e5269b6657524606a0454b4eb
Tim Hsu <timhsu4@gmail.com>
2020-05-18 08:20:52 -0400

    very minor change

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,4 @@
-# miniconda
+# Miniconda
 set MINICONDA_PATH "$HOME/ext/miniconda3"
 if test -d $MINICONDA_PATH
     # >>> conda initialize >>>
@@ -7,7 +7,7 @@ if test -d $MINICONDA_PATH
     # <<< conda initialize <<<
 end
 
-# enable colored 'man'
+# Enable colored 'man'
 set -xU LESS_TERMCAP_md (printf "\e[01;31m")
 set -xU LESS_TERMCAP_me (printf "\e[0m")
 set -xU LESS_TERMCAP_se (printf "\e[0m")
@@ -15,5 +15,5 @@ set -xU LESS_TERMCAP_so (printf "\e[01;44;33m")
 set -xU LESS_TERMCAP_ue (printf "\e[0m")
 set -xU LESS_TERMCAP_us (printf "\e[01;32m")
 
-# aliases
+# Aliases
 alias fzf-pacman="fzf --preview 'pacman -Si {}'"
--- profile/.profile
+++ profile/.profile
@@ -7,7 +7,7 @@ export XDG_CONFIG_HOME="$HOME"/.config
 export XDG_CACHE_HOME="$HOME"/.cache
 export XDG_DATA_HOME="$HOME"/.local/share
 
-# attempt XDG compliance
+# Attempt XDG compliance
 export LESSHISTFILE=-
 export ATOM_HOME="$XDG_DATA_HOME"/atom
 export GNUPGHOME="$XDG_DATA_HOME"/gnupg
@@ -15,7 +15,7 @@ export PYLINTHOME="$XDG_CACHE_HOME"/pylint
 export GTK2_RC_FILES="$XDG_CONFIG_HOME"/gtk-2.0/gtkrc-2.0
 export CUDA_CACHE_PATH="$XDG_CACHE_HOME"/nv
 
-# add julia path
+# Add julia path
 export PATH="$PATH:$HOME/ext/julia-1.4.0/bin"
 
 # fzf customization
